e30a7b24e2ea08283085329ec1d3ee38c6c6fd7d
pabloFuentesVicente <pablo.fuentes-vicente@etu-upsaclay.fr>
2025-04-02 18:13:43 +0200

    Update index.php

--- back/index.php
+++ back/index.php
@@ -5,13 +5,14 @@ require_once 'AuthController.php';
 require_once 'CommentController.php';
 require_once 'RecipeController.php';
 require_once 'RoleController.php';
+require_once 'AuthMiddleware.php';
 
 session_start(); // Start the session
 
 $router = new Router();
 
 
-$authController = new AuthController(__DIR__ . '/data/users.json',__DIR__ . '/data/roles.json' );
+$authController = new AuthController(__DIR__ . '/data/users.json');
 $commentController = new CommentController(__DIR__ . '/data/comments.json', $authController);
 
 //------------------------------------------------------------------
@@ -23,17 +24,16 @@ $recipeController = new RecipeController(__DIR__ . '/data/recipesTest.json');
 //------------------------------------------------------------------
 //Auth
 
-$router->register('POST', '/register', [$authController, 'handleRegister']);
-$router->register('POST', '/login', [$authController, 'handleLogin']);
-$router->register('POST', '/logout', [$authController, 'handleLogout']);
-
+$router->register('POST', '/register', [$authController, 'handleRegister'],false);
+$router->register('POST', '/login', [$authController, 'handleLogin'],false);
+$router->register('POST', '/logout', [$authController, 'handleLogout'],true);
 
 
 //Comments
 
-$router->register('POST', '/comment', [$commentController, 'handlePostCommentRequest']);
-$router->register('GET', '/comment', [$commentController, 'handleGetCommentsRequest']);
-$router->register('DELETE', '/comment', [$commentController, 'handleDeleteCommentRequest']);
+$router->register('POST', '/comment', [$commentController, 'handlePostCommentRequest'],false);
+$router->register('GET', '/comment', [$commentController, 'handleGetCommentsRequest'],false);
+$router->register('DELETE', '/comment', [$commentController, 'handleDeleteCommentRequest'],false);
 
 
 //------------------------------------------------------------------
@@ -42,9 +42,9 @@ $router->register('DELETE', '/comment', [$commentController, 'handleDeleteCommen
 
 //Roles
 */
-$router->register('POST','/askedRoles', [$roleController, 'handleRoleRequest'] );		//Demander un role
-$router->register('POST','/roles',  [$roleController, 'handleRoleApproval'] );		//Accepter la demande d'un role
-$router->register('PUT','/role',  [$roleController, 'handleRoleAssignment']  );		//Attribuer un role
+$router->register('POST','/askedRoles', [$roleController, 'handleRoleRequest'],false );		//Demander un role
+$router->register('POST','/roles',  [$roleController, 'handleRoleApproval'],false );		//Accepter la demande d'un role
+$router->register('PUT','/role',  [$roleController, 'handleRoleAssignment'],false  );		//Attribuer un role
 
 
 
@@ -52,17 +52,17 @@ $router->register('PUT','/role',  [$roleController, 'handleRoleAssignment']  );	
 //Recipe
 
 
-$router->register('POST','/recipe',[$recipeController, 'handleRecipePostProposal']);				//Proposer une recette
-$router->register('DELETE','/recipe/{params}',[$recipeController, 'handleRecipeDeletion']);				//Éliminer une recette
-$router->register('PUT','/recipe',[$recipeController, 'handleRecipeModification']);			//Modifier une recette
+$router->register('POST','/recipe',[$recipeController, 'handleRecipePostProposal'],false);				//Proposer une recette
+$router->register('DELETE','/recipe/{params}',[$recipeController, 'handleRecipeDeletion'],false);				//Éliminer une recette
+$router->register('PUT','/recipe',[$recipeController, 'handleRecipeModification'],false);			//Modifier une recette
 
-$router->register('POST','/recipe/approval/{recipe_name}',[$recipeController, 'handleRecipeApproval']);				//Approuver une recette
-$router->register('GET','/recipe/consult/{recipe_name}',[$recipeController, 'handleRecipeConsulting']);				//Consulter une recette
-$router->register('GET','/recipe/search',[$recipeController, 'handleRecipeSearch']);				//Rechercher une recette
+$router->register('POST','/recipe/approval/{recipe_name}',[$recipeController, 'handleRecipeApproval'],false);				//Approuver une recette
+$router->register('GET','/recipe/consult/{recipe_name}',[$recipeController, 'handleRecipeConsulting'],false);				//Consulter une recette
+$router->register('GET','/recipe/search',[$recipeController, 'handleRecipeSearch'],false);				//Rechercher une recette
 
-$router->register('POST','/recipe/traduction/{params}',[$recipeController, 'handleRecipeTraduction']);				//Traduire une recette
-$router->register('POST','/recipe/photo/{params}',[$recipeController, 'handleRecipeFotoPublication']); 			//Publier une photo d'une recette
-$router->register('POST','/recipe/like/{recipe_name}',[$recipeController, 'handleRecipeLike']);				//Liker une recette
+$router->register('POST','/recipe/traduction/{params}',[$recipeController, 'handleRecipeTraduction'],false);				//Traduire une recette
+$router->register('POST','/recipe/photo/{params}',[$recipeController, 'handleRecipeFotoPublication'],false); 			//Publier une photo d'une recette
+$router->register('POST','/recipe/like/{recipe_name}',[$recipeController, 'handleRecipeLike'],false);				//Liker une recette
 
 
 
@@ -79,4 +79,3 @@ $router->register('GET', '/post/{post_id}/comment/{comment_id}', function ($post
 $router->handleRequest();
 
 ?>
-

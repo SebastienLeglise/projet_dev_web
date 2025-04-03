23018cb238a74c70648cf6fd5824fb63b4c80b91
typhus <sebastien.leglise@etu-upsaclay.fr>
2025-04-03 18:56:46 +0100

    debug

--- api/index.php
+++ api/index.php
@@ -17,7 +17,7 @@ $router = new Router();
 $authController = new AuthController(__DIR__ . '/data/users.json', __DIR__ . '/data/roles.json');
 $commentController = new CommentController(__DIR__ . '/data/comments.json', $authController);
 $roleController = new RoleController(__DIR__ . '/data/roles.json', $authController);
-$recipeController = new RecipeController(__DIR__ . '/data/recipesTest.json');
+$recipeController = new RecipeController(__DIR__ . '/data/recipes.json');
 
 
 //------------------------------------------------------------------
